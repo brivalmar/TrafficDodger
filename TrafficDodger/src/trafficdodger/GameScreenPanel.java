f9e4228a730362a4eadc4cb222a0fa5cf3c2429a
rxr5190 <rxr5190@206-L69.up.ist.local>
2015-11-11 19:58:07 -0500

    Merge origin/master
    Conflicts:
    	TowerDefense/src/TDPlay/TDGrid.java

--- TrafficDodger/src/trafficdodger/GameScreenPanel.java
+++ TrafficDodger/src/trafficdodger/GameScreenPanel.java
@@ -10,6 +10,10 @@ import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
@@ -20,14 +24,17 @@ public class GameScreenPanel extends JPanel implements KeyListener {
 
     private Car car;
     private Player player;
+    BufferedImage road;
 
     GameScreenPanel() {
         super();
         setLayout(null);
+        loadimage();
        // setBackground(Color.BLACK);
         init();
 
         repaint();
+        
     }
 
     void init() {
@@ -42,7 +49,22 @@ public class GameScreenPanel extends JPanel implements KeyListener {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         requestFocusInWindow();
+        drawRoad(g);
         player.draw(g);
+    }
+    
+    void loadimage(){
+        try{
+          road = ImageIO.read(new File("src/trafficdodger/images/road1.png"));
+        }
+        
+        catch(IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public void drawRoad (Graphics g) {
+        g.drawImage(road, 0, 0, null);
     }
 
     @Override
